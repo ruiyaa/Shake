f485f898ea03e2a33bd6123b8c14aa78dcb91429
ruiyaa <nini2525min@gmail.com>
2025-07-12 10:54:00 +0900

    Initial Commit

---
+++ Shake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shake
+//
+//  Created by tanaka niko on 2025/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by tanaka niko on 2025/07/12.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
